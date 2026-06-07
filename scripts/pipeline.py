71dcad931926e17e19df423bd9442fff98879823
Emmanuel-Olasoji <pds533@york.ac.uk>
2026-06-07 14:15:37 +0530

    Delete scripts/pipeline.py as a part of cleanup

--- scripts/pipeline.py
+++
@@ -1,7 +0,0 @@
-"""Backward-compatible wrapper for the modular run pipeline script."""
-
-from scripts.run_pipeline import run_pipeline
-
-
-if __name__ == "__main__":
-    run_pipeline("eanci", ["tfidf", "bm25", "sbert", "codebert"], 10)
